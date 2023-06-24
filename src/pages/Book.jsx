3ac6afe3944e39fee268cbef352c99c3e4040229
Nikolaev Nikita <69678682+NikitkaN@users.noreply.github.com>
2023-06-24 04:28:57 +0300

    fix

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Book.css";
 import GetBook from "./components/GetBook";
 import ReturnBook from "./components/ReturnBook";
+import Table from "./components/Table";
 
 const Book = () => {
     const [ShowReturnBook, setShowReturnBook] = useState(false);
@@ -20,7 +21,7 @@ const Book = () => {
                 <p className="info_head_onHands">на руках</p>
             </div>
             <div className="info_book">
-                <img alt="Book"/>
+                <img src="bookImg/Rectangle_2.png" alt="Book"/>
                 <h2 className="info_book_autor">Э. Голдратт</h2>
                 <h2 className="info_book_name">Критическая цепь</h2>
             </div>
@@ -28,21 +29,7 @@ const Book = () => {
 
         {ShowReturnBook ? <ReturnBook Click={Click} /> : <GetBook Click={Click}/>}
         
-        <div className="history">
-            <h2>Ранее брали</h2>
-            <table width={"100%"}>
-                <tr>
-                    <th align="left" width={"40%"}>nickname</th>
-                    <th align="left" width={"30%"}>взял</th>
-                    <th align="left" width={"30%"}>сдал</th>
-                </tr>
-                <tr>
-                    <td>Lowellda</td>
-                    <td>20.01.2022</td>
-                    <td>27.01.2022</td>
-                </tr>
-            </table>
-        </div>
+        <Table />
         </>
     );
 }
