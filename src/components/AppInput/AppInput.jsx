e0022151b3531fe83fc552170d92135133f6ab6c
popolino <polinadorina888@gmail.com>
2023-07-09 23:33:38 +0300

    Add book and index.css global styles

--- src/components/AppInput/AppInput.jsx
+++ src/components/AppInput/AppInput.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./style.module.css";
+import { clsx } from "clsx";
 
 export function AppInput(props) {
-  return (
-    <div className={style.inputdiv}>
-      <input
-        type="text"
-        className={style.name}
-        required
-        autoComplete="off"
-        value={props.value}
-        onChange={
-          props.onChange
-            ? (e) => {
-                props.onChange(e);
-              }
-            : null
+  const [text, setText] = useState("");
+  const isMobile = false;
+
+  //ниже функция для автоматического ресайза textarea,это нужно, так как в описании книги предположительно будет много текста
+  const textareaRef = useRef();
+  useEffect(() => {
+    const element = textareaRef.current;
+    const autoResizeTextarea = () => {
+      if (element) {
+        element.style.height = element.scrollHeight + "px";
+        if (element.style.height > "50px") {
+          element.style.overflowY = "scroll";
         }
-      />
-      <label className={style.label}>{props.placeholder}</label>
-      <div className={style.bar}></div>
+      }
+    };
+    element && document.addEventListener("input", autoResizeTextarea);
+    return () => {
+      if (element) document.removeEventListener("input", autoResizeTextarea);
+    };
+  }, []);
+
+  return (
+    <div className={style.inputContainer}>
+      {props.description ? (
+        <textarea
+          ref={textareaRef}
+          placeholder={props.title}
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+      ) : (
+        <input
+          className={clsx(style.inputContainer, { [style.mobile]: isMobile })}
+          type="text"
+          placeholder={props.title}
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+      )}
     </div>
   );
 }
