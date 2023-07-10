0b1f20f6f1bc05abb1ee415208f63c55278b152c
Artur Gaiazov <e356mm@gmail.com>
2023-07-10 18:52:33 +0300

    22-APP-BUTTON Update: --return old version change button color from props

--- src/components/AppButton/AppButton.jsx
+++ src/components/AppButton/AppButton.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import style from "./style.module.css";
-import clsx from "clsx";
+// import clsx from "clsx";
 
 export function AppButton(props) {
+  let buttonClass = style.button;
+  if (props.color === "red") {
+    buttonClass += " " + style.red;
+  } else {
+    buttonClass += " " + style.green;
+  }
   return (
     <button
       onClick={props.onClick ? props.onClick : null}
-      className={clsx(style.button, {[style.red]: props.red})}
+      className={buttonClass}
+      // className={clsx(style.button, {[style.red]: props.red})}
     >
       {props.header}
     </button>
