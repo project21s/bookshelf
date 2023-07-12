0e049b24a478c0bdd7407891354a723e07e945a2
Stir <stironni@student.21-school.ru>
2023-07-12 18:13:56 +0300

    промежуточный коммит

--- src/components/AppInput/AppInput.jsx
+++ src/components/AppInput/AppInput.jsx
@@ -1,25 +1,72 @@
-import React from "react";
+// import React from "react";
+// import style from "./style.module.css";
+
+// export function AppInput(props) {
+//   return (
+//     <div className={style.inputdiv}>
+//       <input
+//         type="text"
+//         className={style.name}
+//         required
+//         autoComplete="off"
+//         value={props.value}
+//         onChange={
+//           props.onChange
+//             ? (e) => {
+//                 props.onChange(e);
+//               }
+//             : null
+//         }
+//       />
+//       <label className={style.label}>{props.placeholder}</label>
+//       <div className={style.bar}></div>
+//     </div>
+//   );
+// }
+
+import React, { useEffect, useRef, useState } from "react";
 import style from "./style.module.css";
+import { clsx } from "clsx";
 
 export function AppInput(props) {
+  const [text, setText] = useState("");
+  const isMobile = true;
+
+  //ниже функция для автоматического ресайза textarea,это нужно, так как в описании книги предположительно будет много текста
+  const textareaRef = useRef();
+    const [height, setHeight] = useState(0)
+    useEffect(() => {
+    const element = textareaRef.current;
+    if (!element) return
+    setHeight(element.scrollHeight)
+    const autoResizeTextarea = () => {
+        setHeight(element.scrollHeight)
+    };
+    document.addEventListener("input", autoResizeTextarea);
+    return () => {
+   document.removeEventListener("input", autoResizeTextarea);
+    };
+  }, []);
+
   return (
-    <div className={style.inputdiv}>
-      <input
-        type="text"
-        className={style.name}
-        required
-        autoComplete="off"
-        value={props.value}
-        onChange={
-          props.onChange
-            ? (e) => {
-                props.onChange(e);
-              }
-            : null
-        }
-      />
-      <label className={style.label}>{props.placeholder}</label>
-      <div className={style.bar}></div>
+    <div className={clsx(style.inputContainer, {[style.scroll] : height > 50})}>
+      {props.description ? (
+        <textarea
+          style={{height:`${height}px`}}
+          ref={textareaRef}
+          placeholder={props.title}
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+      ) : (
+        <input
+          className={clsx(style.inputContainer, { [style.mobile]: isMobile })}
+          type="text"
+          placeholder={props.title}
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+      )}
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 import style from "./style.module.css";
-import { AppButton } from "../../components/AppButton/AppButton";
-import { Player } from "@lottiefiles/react-lottie-player";
-import spaceman from "../../assets/spaceman.json";
 import { books } from "../../services/bookMockAPI";
 
 import BookTable from "../../components/BookTable";
 import SearchBar from "../../components/SearchBar";
-
 function Home() {
   const [filterText, setFilterText] = useState("");
   const [inStockOnly, setInStockOnly] = useState(false);
 
-  let navigate = useNavigate();
-
-  const goAddBook = () => {
-    navigate("/addbook");
-  };
+  // let navigate = useNavigate();
 
   return (
-    <div>
-      <div className={style.main}>
-        <div className={style.addbook}>
-          <AppButton size="s" header="Добавить книгу" onClick={goAddBook} />
-        </div>
-        <div className={style.header}>
-          <Player autoplay loop src={spaceman}></Player>
+    <div className={style.main}>
+      <div className={style.blockLeft}>
+        <div className={style.hello}>
           <span>Рады видеть тебя в нашей школьной библиотеке</span>
         </div>
         <div className={style.info}>
           <div>
-            Найди интересную тебе книгу, если она сейчас свободна - выбери ее в
-            списке и укажи свой ник
-          </div>
-          <br />
-          <div>- Брать книгу на одну неделю, не больше</div>
-          <div>- Книги просим возвращать в свою ячейку согласно её номеру</div>
+              Найди интересную тебе книгу, если она сейчас свободна - выбери ее в
+              списке и укажи свой ник
+            </div>
+            <br />
+            <div>- Брать книгу на одну неделю, не больше</div>
+            <div>- Книги просим возвращать в свою ячейку согласно её номеру</div>
         </div>
         <SearchBar
           filterText={filterText}
@@ -46,15 +34,11 @@ function Home() {
           onFilterTextChange={setFilterText}
           onInStockOnlyChange={setInStockOnly}
         />
-      </div>
-
-      <div>
-        <BookTable
+      </div>        <BookTable className={style.bookTable}
           books={books}
           filterText={filterText}
           inStockOnly={inStockOnly}
         />
-      </div>
     </div>
   );
 }
