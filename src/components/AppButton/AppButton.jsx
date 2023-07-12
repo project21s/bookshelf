603963f1edee3e1e88ccf5e0c5a931158759536f
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2023-07-13 01:35:17 +0300

    fix: app button

--- src/components/AppButton/AppButton.jsx
+++ src/components/AppButton/AppButton.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import style from "./style.module.css";
-// import clsx from "clsx";
+import clsx from "clsx";
 
 export function AppButton(props) {
-  let buttonClass = style.button;
-  if (props.color === "red") {
-    buttonClass += " " + style.red;
-  } else {
-    buttonClass += " " + style.green;
-  }
   return (
     <button
       onClick={props.onClick ? props.onClick : null}
-      className={buttonClass}
-      // className={clsx(style.button, {[style.red]: props.red})}
+      className={clsx(style.button, { [style.red]: props.red })}
     >
       {props.header}
     </button>
