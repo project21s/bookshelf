9f2dfb211f41831fdb3988704ae3287e34763747
stironni <stironni@student.21-school.ru>
2023-07-13 19:53:20 +0300

    fix: adaptive layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,20 +6,20 @@ import style from "./style.module.css";
 import { books } from "../../services/bookMockAPI";
 
 import BookTable from "../../components/BookTable";
-import SearchBar from "../../components/SearchBar";
+import AppSearch from "../../components/AppSearch/AppSearch";
+import { toHaveFormValues } from "@testing-library/jest-dom/dist/matchers";
 function Home() {
   const [filterText, setFilterText] = useState("");
   const [inStockOnly, setInStockOnly] = useState(false);
 
-  const isMobile = true;
+  const isMobile = toHaveFormValues;
 
   const main = (isMobile) ? style.mobile : style.dekstop;
 
   // let navigate = useNavigate();
-
   return (
     <div className={main}>
-      <div className={style.blockLeft} id="one">
+      <div className={style.blockLeft}>
         <div className={style.hello}>
           <span>Рады видеть тебя в нашей школьной библиотеке</span>
         </div>
@@ -32,7 +32,9 @@ function Home() {
             <div>- Брать книгу на одну неделю, не больше</div>
             <div>- Книги просим возвращать в свою ячейку согласно её номеру</div>
         </div>
-        <SearchBar
+      </div>
+      <div className={style.search}>
+        <AppSearch 
           filterText={filterText}
           inStockOnly={inStockOnly}
           onFilterTextChange={setFilterText}
@@ -45,9 +47,50 @@ function Home() {
           filterText={filterText}
           inStockOnly={inStockOnly}
         />
-      </div>        
-      
-    </div>
+      </div> 
+    </div>       
+
+
+
+/* <div className={main}>
+<div className={style.blockLeft} id="one">
+  <div className={style.hello}>
+    <span>Рады видеть тебя в нашей школьной библиотеке</span>
+  </div>
+  <div className={style.info}>
+    <div>
+        Найди интересную тебе книгу, если она сейчас свободна - выбери ее в
+        списке и укажи свой ник
+      </div>
+      <br />
+      <div>- Брать книгу на одну неделю, не больше</div>
+      <div>- Книги просим возвращать в свою ячейку согласно её номеру</div>
+  </div>
+  <div className={style.hiddenIfMobile}>
+  <SearchBar 
+    filterText={filterText}
+    inStockOnly={inStockOnly}
+    onFilterTextChange={setFilterText}
+    onInStockOnlyChange={setInStockOnly}
+  />
+  </div>
+</div>
+<div className={style.fixSearch}>
+  <SearchBar 
+    filterText={filterText}
+    inStockOnly={inStockOnly}
+    onFilterTextChange={setFilterText}
+    onInStockOnlyChange={setInStockOnly}
+  />
+  </div>
+<div className={style.bookTable}>
+<BookTable 
+    books={books}
+    filterText={filterText}
+    inStockOnly={inStockOnly}
+  />
+</div>        
+</div>  */
   );
 }
 
