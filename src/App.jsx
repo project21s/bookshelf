b2ff9635ac066ea915016eefbf5b4828ebcb41cb
popolino <polinadorina888@gmail.com>
2023-07-13 14:50:23 +0300

    создание header и мобильной навигационной боковой панели, создание компоненты svgSelector для удобного использования svg, подключение react transition group, добавление глобальных стилей, создание appReducer, вынесение роутов в массив

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Header from "./components/Header/Header";
 
 export const App = () => {
   const { isMobile } = useScreen();
+
   return (
     <div className={clsx({ ["mobile"]: isMobile })}>
       <BrowserRouter>
