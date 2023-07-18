fd2c98a47b490d1240332f8ba682bbab4e3b3af4
Stir <stironni@student.21-school.ru>
2023-07-18 10:55:34 +0300

    fix: scroll button up (new version)

---
+++ src/components/AppScrollToUp/AppScrollToUp.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react";
+import AppButton from "../AppButton/AppButton";
+
+function AppScrollToUp() {
+const [showTopBtn, setShowTopBtn] = useState(false);
+useEffect(() => {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            setShowTopBtn(true);
+        } else {
+            setShowTopBtn(false);
+        }
+    });
+}, []);
+const goToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
+
+return (
+    <div>
+    {" "}
+            {showTopBtn && (
+         
+      <AppButton scrollUp onClick={goToTop} />)}{" "}
+    </div>
+);
+}
+
+export default AppScrollToUp;
