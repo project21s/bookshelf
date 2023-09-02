f8e803d95d3aa27587921b753a877502078703d7
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2023-09-03 00:36:10 +0300

    add techWork and 404 pades

---
+++ src/pages/TechWork/Feedback.jsx
@@ -0,0 +1,15 @@
+import style from "./style.module.css";
+
+function TechWork() {
+  return (
+    <div className={style.main}>
+      <div className={style.content}>
+        <h2>Становимся лучше</h2>
+        <br />
+        <h3>Мы ушли на технические работы и скоро вернемся </h3>
+      </div>
+    </div>
+  );
+}
+
+export default TechWork;
