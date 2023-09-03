fd83fee0b76da3e412bba05d0976249894c4b2ef
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2023-09-03 05:49:56 +0300

    add telegram feetback

--- src/components/AppImageDownload/AppImageDownload.jsx
+++ src/components/AppImageDownload/AppImageDownload.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import style from "./style.module.css";
 
-
-export function AppImageDownload({ file, setFile }) {
+export function AppImageDownload({ file, setFile, title }) {
   function handleChange(e) {
     let img = e.target.files[0];
     setFile(img);
   }
   return (
     <div className={style.loadImage} onChange={handleChange}>
-      {file ? <img src={URL.createObjectURL(file)} alt="" /> : <p>Загрузить фото книги</p>}
+      {file ? (
+        <img src={URL.createObjectURL(file)} alt="prev" />
+      ) : (
+        <p>{title}</p>
+      )}
       <input
         className={style.inputFile}
         type="file"
--- src/services/feedbackServes.js
+++ src/services/feedbackServes.js
@@ -1,29 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebaseConfig";
-import {
-  getFirestore,
-  setDoc,
-  doc,
-  getDoc,
-  collection,
-  addDoc,
-  updateDoc,
-  getDocs,
-} from "firebase/firestore";
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export const addFeedback = async (text) => {
+/**
+ * Отправляет отзыв в телеграм бот
+ * @param  text текст сообщения
+ * @param  file фото сообщения
+ * @return объект со статусом запроса
+ */
+export const addFeedback = async (text, file) => {
   let feedback = {};
   try {
-    let date = new Date();
-    const docRef = await addDoc(collection(db, "feedback"), {
-      date: date,
-      text: text,
-      status: "open",
-    });
-
+    text ? (text = text) : (text = "__только фото__");
+    let str = `https://api.telegram.org/bot${process.env.REACT_APP_TG_TOKEN}/sendMessage?chat_id=406153580&text=${text}`;
+    await fetch(str);
+    if (file) {
+      const formData = new FormData();
+      formData.append("photo", file);
+      let str2 = `https://api.telegram.org/bot${process.env.REACT_APP_TG_TOKEN}/sendPhoto?chat_id=406153580`;
+      await fetch(str2, {
+        method: "POST",
+        body: formData,
+      });
+    }
     feedback.status = "ok";
   } catch (e) {
     feedback.status = "error";
